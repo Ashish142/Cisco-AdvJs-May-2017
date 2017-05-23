a605b1634c2e0ca3d37033a656af74d11600d5dd
Magesh <tkmagesh77@gmail.com>
2017-05-23 12:30:03 +0530

    spinner assignment

---
+++ 02-closures/spinner.js
@@ -0,0 +1,28 @@
+//Create an object and assign it to a variable called 'spinner'
+var spinner = /* **** */
+
+
+/*
+The spinner object must exhibit the following behavious
+
+spinner.up(); //=> 1
+spinner.up(); //=> 2
+spinner.up(); //=> 3
+spinner.up(); //=> 4
+
+spinner.down(); //=> 3
+spinner.down(); //=> 2
+spinner.down(); //=> 1
+spinner.down(); //=> 0
+spinner.down(); //=> -1
+
+The varible used the track the value should NOT be accessible from outside the spinner object
+*/
+
+
+var emp = {
+	name : 'magesh',
+	print : function(){
+		console.log('my name is ', this.name);
+	}
+}
