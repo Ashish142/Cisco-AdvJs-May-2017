16213e6f3d93fb122ba4f23a882cc8d48652a33d
Magesh <tkmagesh77@gmail.com>
2017-05-24 10:02:35 +0530

    isPrime - cached version

--- 02-closures/isPrime.js
+++ 02-closures/isPrime.js
@@ -8,8 +8,64 @@ function isPrime(n){
 	return true;
 }
 
+//public cache
+function isPrime(n){
+	if (typeof isPrime.map === 'undefined')
+		isPrime.map = {};
+	var map = isPrime.map;
+    if (typeof map[n] !== 'undefined')
+        return map[n];
+    console.log('processing ', n);
+    map[n] = true;
+    for(var index = 2; index <= (n/2); index++)
+        if (n % index === 0){
+            map[n] = false;
+            break;
+        }
+    return map[n];
+}
 
-isPrime(100)
-isPrime(101)
 
-isPrime(100)
+//private cache
+var isPrime = (function (){
+    var map = {};
+
+    function isPrime(n){
+        if (typeof map[n] !== 'undefined')
+            return map[n];
+        console.log('processing ', n);
+        map[n] = true;
+        for(var index = 2; index <= (n/2); index++)
+            if (n % index === 0){
+                map[n] = false;
+                break;
+            }
+        return map[n];
+    }
+	return isPrime;
+})();
+
+
+var isPrime = (function (){
+
+    var map = {};
+    
+    function checkPrime(n){
+    	console.log('processing ', n);
+    	if (n <= 3) return true;
+    	for(var index = 2; index <= (n/2); index++)
+            if (n % index === 0){
+                return false;
+            }
+    }
+
+    return function(n){
+        if (typeof map[n] === 'undefined')
+            map[n] = checkPrime(n);
+        return map[n];
+    }
+})();
+
+
+
+
