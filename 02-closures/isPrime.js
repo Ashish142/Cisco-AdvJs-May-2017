129bcbb7abb61ee9c1b129e0590b9bfb999da4a0
Magesh <tkmagesh77@gmail.com>
2017-05-24 10:21:07 +0530

    memoize implementation

--- 02-closures/isPrime.js
+++ 02-closures/isPrime.js
@@ -66,6 +66,62 @@ var isPrime = (function (){
     }
 })();
 
+var isOddOrEven = (function (){
+
+    var map = {};
+    
+    function checkOddOrEven(n){
+    	console.log('processing ', n);
+    	return n % 2 === 0 ? 'even' : 'odd';
+    }
+
+    return function(n){
+        if (typeof map[n] === 'undefined')
+            map[n] = checkOddOrEven(n);
+        return map[n];
+    }
+})();
+
+
+function memoize(fn){
+    var map = {};
+    return function(n){
+        if (typeof map[n] === 'undefined')
+            map[n] = fn(n);
+        return map[n];
+    }
+}
+
+var oddOrEven = memoize(function(n){
+	console.log('processing ', n);
+	return n % 2 === 0 ? 'even' : 'odd';
+});
+
+var isPrime = memoize(function(n){
+	console.log('processing ', n);
+	if (n <= 3) return true;
+	for(var index = 2; index <= (n/2); index++)
+        if (n % index === 0){
+            return false;
+        }
+});
+
+function memoize(fn){
+    var map = {};
+    return function(){
+    	var key = JSON.stringify(arguments);
+        if (typeof map[key] === 'undefined')
+            map[key] = fn.apply(this, arguments);
+        return map[key];
+    }
+}
+
+
+
+
+
+
+
 
 
 
